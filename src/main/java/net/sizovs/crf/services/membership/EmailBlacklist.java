7fab32afa636fafa085f159cfe16cd9447e67e76
Eduards Sizovs <eduards.sizovs@gmail.com>
2019-01-20 19:34:12 +0200

    switch to List.of(...)

--- src/main/java/net/sizovs/crf/services/membership/EmailBlacklist.java
+++ src/main/java/net/sizovs/crf/services/membership/EmailBlacklist.java
@@ -2,16 +2,15 @@ package net.sizovs.crf.services.membership;
 
 import org.springframework.stereotype.Component;
 
-import java.util.Arrays;
 import java.util.List;
 
 @Component
 public class EmailBlacklist {
 
-    private final List<String> badDomains = Arrays.asList("pornhub.com", "rotten.com");
+    private static final List<String> BAD_DOMAINS = List.of("pornhub.com", "rotten.com");
 
     public boolean contains(Email email) {
-        return badDomains.stream().anyMatch(domain -> email.toString().contains(domain));
+        return BAD_DOMAINS.stream().anyMatch(domain -> email.toString().contains(domain));
     }
 
 }
