583b2c3d6a196e80000ae4e6c7043b82a0076c69
Eduards Sizovs <eduards@devternity.com>
2019-07-25 17:32:58 +0300

    organize imports

--- src/test/groovy/lightweight4j/features/membership/BecomeAMemberSpec.groovy
+++ src/test/groovy/lightweight4j/features/membership/BecomeAMemberSpec.groovy
@@ -1,22 +1,17 @@
 package lightweight4j.features.membership
 
-import org.hamcrest.Description
-import org.hamcrest.Matcher
-import org.hamcrest.TypeSafeMatcher
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
-
-import static StringMatchesUUIDPattern.matchesUuid
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
-
 import org.springframework.test.web.servlet.MockMvc
 import spock.lang.Specification
 
+import static StringMatchesUUIDPattern.matchesUuid
 import static groovy.json.JsonOutput.toJson
 import static org.springframework.http.MediaType.APPLICATION_JSON
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status
 
 @SpringBootTest
