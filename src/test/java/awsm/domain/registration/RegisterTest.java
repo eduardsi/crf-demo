2da09b614d277cd8ec19c96fcf2f947d34a53eea
Eduards Sizovs <eduards@devternity.com>
2019-08-18 19:40:31 +0300

    better tests

--- src/test/java/awsm/domain/registration/RegisterTest.java
+++ src/test/java/awsm/domain/registration/RegisterTest.java
@@ -36,15 +36,11 @@ class RegisterTest {
   @Autowired
   private MockMvc mvc;
 
-  private Faker faker = new Faker();
-
   @Test
-  void rate_limits_to_one_registration_at_a_time()  {
-
+  void rate_limits_to_one_registration_at_a_time() {
     var executor = Executors.newFixedThreadPool(2);
-
-    var responses = Stream.of(fakeEmail(), fakeEmail())
-        .collect(parallelToList(this::registerWithEmail, executor))
+    var responses = Stream.of(email(), email())
+        .collect(parallelToList(this::register, executor))
         .join()
         .stream()
         .map(ResultActions::andReturn)
@@ -53,7 +49,6 @@ class RegisterTest {
 
     assertThat(responses).haveAtLeastOne(new Condition<>(ex -> ex.getStatus() == 200, "status OK"));
     assertThat(responses).haveAtLeastOne(new Condition<>(ex -> ex.getStatus() == 429, "status too many requests"));
-
   }
 
   @Test
@@ -65,64 +60,52 @@ class RegisterTest {
 
   @Test
   void throws_if_email_is_missing() throws Exception {
-    var json = "{" + "\"firstName\": \"Eduards\"," + "\"lastName\": \"Sizovs\"" + "}";
-    mvc.perform(post("/members")
-            .content(json)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is(400))
-            .andExpect(jsonPath("$.[0].property", is("email")))
-            .andExpect(jsonPath("$.[0].message", is("must not be empty")));
+    register("", "Eduards", "Sizovs")
+        .andExpect(status().is(400))
+        .andExpect(jsonPath("$.[0].property", is("email")))
+        .andExpect(jsonPath("$.[0].message", is("must not be empty")));
   }
 
   @Test
   void throws_if_first_name_is_missing() throws Exception {
-    var json = "{" + "\"email\": \"eduards@sizovs.net\"," + "\"lastName\": \"Sizovs\"" + "}";
-    mvc.perform(post("/members")
-            .content(json)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is(400))
-            .andExpect(jsonPath("$.[0].property", is("firstName")))
-            .andExpect(jsonPath("$.[0].message", is("must not be empty")));
+    register(email(), "", "Sizovs")
+        .andExpect(status().is(400))
+        .andExpect(jsonPath("$.[0].property", is("firstName")))
+        .andExpect(jsonPath("$.[0].message", is("must not be empty")));
   }
 
   @Test
   void throws_if_last_name_is_missing() throws Exception {
-    var json = "{" + "\"email\": \"eduards@sizovs.net\"," + "\"firstName\": \"Eduards\"" + "}";
-    mvc.perform(post("/members")
-            .content(json)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(status().is(400))
-            .andExpect(jsonPath("$.[0].property", is("lastName")))
-            .andExpect(jsonPath("$.[0].message", is("must not be empty")));
+    register(email(), "Eduards", "")
+        .andExpect(status().is(400))
+        .andExpect(jsonPath("$.[0].property", is("lastName")))
+        .andExpect(jsonPath("$.[0].message", is("must not be empty")));
   }
 
   @ParameterizedTest
   @ValueSource(strings = {"pornhub.com", "rotten.com"})
   void throws_if_email_is_blacklisted(String domain) throws Exception {
     var email = "eduards@" + domain;
-    var json = "{" + "\"email\": \"" + email + "\"" + "," + "\"firstName\": \"Eduards\"," + "\"lastName\": \"Sizovs\"" + "}";
-    mvc.perform(post("/members")
-            .content(json)
-            .accept(MediaType.APPLICATION_JSON)
-            .contentType(MediaType.APPLICATION_JSON))
-            .andExpect(content().string("Email " + email + " is blacklisted"))
-            .andExpect(status().is(400));
+    register(email, "Eduards", "Sizovs")
+        .andExpect(status().is(400))
+        .andExpect(content().string("Email " + email + " is blacklisted"));
   }
 
   private ResultActions register() {
-    String email = fakeEmail();
-    return registerWithEmail(email);
+    return this.register(email());
   }
 
-  private String fakeEmail() {
-    return faker.internet().emailAddress();
+  private ResultActions register(String email) {
+    return register(email, "Eduards", "Sizovs");
   }
 
-  private ResultActions registerWithEmail(String email) {
-    var json = "{" + "\"email\": \"" + email + "\"," + "\"firstName\": \"Eduards\"," + "\"lastName\": \"Sizovs\"" + "}";
+  private String email() {
+    return new Faker().internet().emailAddress();
+  }
+
+  private ResultActions register(String email, String firstName, String lastName) {
+    var json = "{" + "\"email\": \"" + email + "\"," + "\"firstName\": \""
+        + firstName + "\"," + "\"lastName\": \"" + lastName + "\"" + "}";
     return sneak().get(() -> mvc.perform(post("/members")
         .content(json)
         .accept(MediaType.APPLICATION_JSON)
