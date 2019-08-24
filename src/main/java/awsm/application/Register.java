63b6930f07f01b8bb39e8690d42aaac3f93a8be7
Eduards Sizovs <eduards@devternity.com>
2019-08-24 22:02:39 +0300

    make Welcome part of Register

--- src/main/java/awsm/application/Register.java
+++ src/main/java/awsm/application/Register.java
@@ -1,6 +1,7 @@
 package awsm.application;
 
 import static awsm.infra.memoization.Memoizers.memoizer;
+import static awsm.infra.middleware.ReturnsNothing.NOTHING;
 import static org.springframework.beans.factory.config.BeanDefinition.SCOPE_PROTOTYPE;
 
 import awsm.domain.administration.Administrator;
@@ -14,9 +15,11 @@ import awsm.domain.registration.Name;
 import awsm.infra.hashing.HashId;
 import awsm.infra.jackson.JacksonConstructor;
 import awsm.infra.middleware.Command;
+import awsm.infra.middleware.ReturnsNothing;
 import awsm.infra.middleware.impl.react.Reaction;
 import awsm.infra.middleware.impl.react.validation.Validator;
 import awsm.infra.middleware.impl.resilience.RateLimit;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
@@ -116,4 +119,29 @@ class Register implements Command<CharSequence> {
 
   }
 
+  static class Welcome implements Command<ReturnsNothing> {
+
+    private final long memberId;
+
+    Welcome(@JsonProperty("memberId") long memberId) {
+      this.memberId = memberId;
+    }
+
+    @Component
+    static class Re implements Reaction<Welcome, ReturnsNothing> {
+
+      private final Members members;
+
+      private Re(Members members) {
+        this.members = members;
+      }
+
+      @Override
+      public ReturnsNothing react(Welcome cmd) {
+        var member = members.findById(cmd.memberId).orElseThrow();
+        System.out.printf("Sending email to %s: Welcome to the Matrix, %s", member.email(), member.name());
+        return NOTHING;
+      }
+    }
+  }
 }
