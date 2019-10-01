70d4701ecf8f17e982039a45f9fdcd3c0665a3ae
Eduards Sizovs <eduards@devternity.com>
2019-10-01 15:21:21 +0300

    marge schedulers into fewer classes

--- src/main/java/awsm/infra/middleware/impl/scheduler/ScheduledCommand.java
+++ src/main/java/awsm/infra/middleware/impl/scheduler/ScheduledCommand.java
@@ -1,14 +1,19 @@
 package awsm.infra.middleware.impl.scheduler;
 
 import static com.google.common.base.Preconditions.checkState;
+import static com.machinezoo.noexception.Exceptions.sneak;
 import static java.time.ZoneOffset.UTC;
 
 import awsm.infra.hibernate.HibernateConstructor;
 import awsm.infra.middleware.Command;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.time.LocalDateTime;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import javax.annotation.Nullable;
+import javax.persistence.AttributeConverter;
 import javax.persistence.Column;
 import javax.persistence.Convert;
 import javax.persistence.Entity;
@@ -68,6 +73,26 @@ class ScheduledCommand {
   @Override
   public String toString() {
     return ReflectionToStringBuilder.toString(this, ToStringStyle.SIMPLE_STYLE);
+  }
+
+  private static class CommandConverter implements AttributeConverter<Command, String> {
+
+    private static final ObjectMapper mapper = new ObjectMapper();
+
+    static {
+      mapper.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+      mapper.setVisibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY);
+    }
+
+    @Override
+    public String convertToDatabaseColumn(Command attribute) {
+      return sneak().get(() -> mapper.writeValueAsString(attribute));
+    }
+
+    @Override
+    public Command convertToEntityAttribute(String command) {
+      return sneak().get(() -> mapper.readValue(command, Command.class));
+    }
   }
 
 }
